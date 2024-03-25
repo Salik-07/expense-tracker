14d07652ce44fc0b0d240ada211858369cf22667
Salik-07 <salikhussain41@gmail.com>
2024-03-25 16:49:21 +0500

    Add expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+import ExpenseList from "./components/ExpenseList";
+
 function App() {
-  return <h1>Vite + React</h1>;
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  return (
+    <ExpenseList
+      expenses={expenses}
+      onDelete={(id) =>
+        setExpenses(expenses.filter((expense) => expense.id !== id))
+      }
+    />
+  );
 }
 
 export default App;
